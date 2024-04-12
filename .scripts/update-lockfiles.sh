c143ffd2df3d821c47ec1208493ea96d7680717b
FabianLars <fabianlars@fabianlars.de>
2024-04-12 23:22:25 +0200

    chore: remove bench project

--- .scripts/update-lockfiles.sh
+++ .scripts/update-lockfiles.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: MIT
 
 declare -a examples=("api" "sidecar" "resources" "tauri-dynamic-lib" "workspace")
-declare -a tooling=("bench" "cli")
+declare -a tooling=("cli")
 
 for example in "${examples[@]}"
 do
